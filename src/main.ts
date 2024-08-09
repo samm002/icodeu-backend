33eb4cacb58830cc9f69259835419c22c1505b55
samm002 <samuelsdjodis@gmail.com>
2024-08-10 00:33:36 +0700

    feat(swagger): adding swagger for endpoint testing

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
@@ -17,8 +21,17 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
+  const config = new DocumentBuilder()
+    .setTitle('IcodeU Endpoint Test')
+    .setDescription('IcodeU Endpoint Testing')
+    .setVersion('1.0')
+    .addTag('icodeu')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`API is running on: ${await app.getUrl()}`);
 }
 bootstrap();
